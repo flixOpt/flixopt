0a6415f732294026d02372a6350a2fe2eb6a80c4
FBumann <117816358+FBumann@users.noreply.github.com>
2025-03-15 18:38:07 +0100

    Move math from classes to docs

--- docs/api/components/storage.md
+++ docs/api/components/storage.md
@@ -1,6 +1,40 @@
+# Mathematical Formulation
+
+**Storages** have one incoming and one outgoing **Flow** - $f_\text{in}$ and $f_\text{out}$ -
+each with an efficiency $\eta_\text{in}$ and $\eta_\text{out}$.
+Further, storages have a `size` $\text C$ and a state of charge $c(\text{t}_i)$.
+Similarly to the flow-rate $p(\text{t}_i)$ of a [`Flow`][flixOpt.elements.Flow],
+the `size` $\text C$ combined with a relative upper bound
+$\text c^{\text{U}}_\text{rel}(\text t_{i})$ and lower bound $\text c^{\text{L}}_\text{rel}(\text t_{i})$
+limits the state of charge $c(\text{t}_i)$ by $\eqref{eq:Storage_Bounds}$.
+
+$$ \label{eq:Storage_Bounds}
+    \text C \cdot \text c^{\text{L}}_{\text{rel}}(\text t_{i})
+    \leq c(\text{t}_i) \leq
+    \text C \cdot \text c^{\text{U}}_{\text{rel}}(\text t_{i})
+$$
+
+With $\text c^{\text{L}}_{\text{rel}}(\text t_{i}) = 0$ and $\text c^{\text{U}}_{\text{rel}}(\text t_{i}) = 1$,
+Equation $\eqref{eq:Storage_Bounds}$ simplifies to
+
+$$ 0 \leq c(\text t_{i}) \leq \text C $$
+
+The state of charge $c(\text{t}_i)$ decreases by a fraction of the prior state of charge. The belonging parameter
+$ \dot{ \text c}_\text{rel, loss}(\text{t}_i)$ expresses the "loss fraction per hour". The storage balance from  $\text{t}_i$ to $\text t_{i+1}$ is
+
+$$
+\begin{align*}
+    c(\text{t}_{i+1}) &= c(\text{t}_{i}) \cdot (1-\dot{\text{c}}_\text{rel,loss}(\text{t}_i) \cdot \Delta \text{t}_{i}) \\
+    &\quad + p_{f_\text{in}}(\text{t}_i) \cdot \Delta \text{t}_i \cdot \eta_\text{in}(\text{t}_i) \\
+    &\quad - \frac{p_{f_\text{out}}(\text{t}_i) \cdot \Delta \text{t}_i}{\eta_\text{out}(\text{t}_i)}
+    \tag{3}
+\end{align*}
+$$
+
 ::: flixOpt.components.Storage
 
-### Creating a Storage
+
+## Creating a Storage
 
 ```python
 import flixOpt as fx
--- flixOpt/components.py
+++ flixOpt/components.py
@@ -128,44 +128,6 @@ class LinearConverter(Component):
 
 @register_class_for_io
 class Storage(Component):
-    r"""
-    **Storages** have one incoming and one outgoing **Flow** - $f_\text{in}$ and $f_\text{out}$ -
-    each with an efficiency $\eta_\text{in}$ and $\eta_\text{out}$. Further, storages have a size $\text C$ and a
-    state of charge $c(\text{t}_i)$.
-    Similarly to the flow-rate $p(\text{t}_i)$ of a `Flow`, the `size` $\text C$ combined with a relative upper bound
-    $\text c^{\text{U}}_\text{rel}(\text t_{i})$ and lower bound $\text c^{\text{L}}_\text{rel}(\text t_{i})$
-    limits the state of charge $c(\text{t}_i)$ by $\eqref{eq:Storage_Bounds}$.
-
-    $$ \label{eq:Storage_Bounds}
-        \text C \cdot \text c^{\text{L}}_{\text{rel}}(\text t_{i})
-        \leq c(\text{t}_i) \leq
-        \text C \cdot \text c^{\text{U}}_{\text{rel}}(\text t_{i})
-    $$
-
-    With $\text c^{\text{L}}_{\text{rel}}(\text t_{i}) = 0$ and $\text c^{\text{U}}_{\text{rel}}(\text t_{i}) = 1$,
-    Equation $\eqref{eq:Storage_Bounds}$ simplifies to
-
-    $$ 0 \leq c(\text t_{i}) \leq \text C $$
-
-    The state of charge $c(\text{t}_i)$ decreases by a fraction of the prior state of charge. The belonging parameter
-    $ \dot{ \text c}_\text{rel, loss}(\text{t}_i)$ expresses the "loss fraction per hour". The storage balance from  $\text{t}_i$ to $\text t_{i+1}$ is
-
-    $$
-    \begin{align*}
-        c(\text{t}_{i+1}) &= c(\text{t}_{i}) \cdot (1-\dot{\text{c}}_\text{rel,loss}(\text{t}_i) \cdot \Delta \text{t}_{i}) \\
-        &\quad + p_{f_\text{in}}(\text{t}_i) \cdot \Delta \text{t}_i \cdot \eta_\text{in}(\text{t}_i) \\
-        &\quad - \frac{p_{f_\text{out}}(\text{t}_i) \cdot \Delta \text{t}_i}{\eta_\text{out}(\text{t}_i)}
-        \tag{3}
-    \end{align*}
-    $$
-    """
-
-    # TODO: Dabei fällt mir auf. Vielleicht sollte man mal überlegen, ob man für Ladeleistungen bereits in dem
-    #  jeweiligen Zeitschritt mit einem Verlust berücksichtigt. Zumindest für große Zeitschritte bzw. große Verluste
-    #  eventuell relevant.
-    #  -> Sprich: speicherverlust = charge_state(t) * relative_loss_per_hour * dt + 0.5 * Q_lade(t) * dt * relative_loss_per_hour * dt
-    #  -> müsste man aber auch für den sich ändernden Ladezustand berücksichtigten
-
     def __init__(
         self,
         label: str,
@@ -184,6 +146,13 @@ class Storage(Component):
         meta_data: Optional[Dict] = None,
     ):
         """
+        Storages have one incoming and one outgoing Flow each with an efficiency.
+        Further, storages have a `size` and a `charge_state`.
+        Similarly to the flow-rate of a Flow, the `size` combined with a relative upper and lower bound
+        limits the `charge_state` of the storage.
+
+        For mathematical details take a look at our online documentation
+
         Args:
             label: The label of the Element. Used to identify it in the FlowSystem
             charging: ingoing flow.
