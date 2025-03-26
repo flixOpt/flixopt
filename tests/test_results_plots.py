0b30ff3c964e6f949955d718c1c1f492e7ce1964
FBumann <117816358+FBumann@users.noreply.github.com>
2025-03-26 11:48:24 +0100

    Add tests

---
+++ tests/test_results_plots.py
@@ -0,0 +1,57 @@
+import pytest
+
+import flixOpt as fx
+
+from .conftest import create_calculation_and_solve, simple_flow_system
+
+
+@pytest.fixture(params=[True, False])
+def show(request):
+    return request.param
+
+
+@pytest.fixture(params=[True, False])
+def save(request):
+    return request.param
+
+
+def test_results_plots_matplotlib(simple_flow_system, show, save):
+    calculation = create_calculation_and_solve(simple_flow_system, fx.solvers.HighsSolver(0.01, 30), 'test_results_plots')
+    results = calculation.results
+
+    results['Boiler'].plot_node_balance(engine='matplotlib', save=save, show=show)
+
+    results.plot_heatmap('Speicher(Q_th_load)|flow_rate',
+                         heatmap_timeframes='D',
+                         heatmap_timesteps_per_frame='h',
+                         color_map='viridis',
+                         save=show,
+                         show=save,
+                         engine='matplotlib')
+
+    with pytest.raises(NotImplementedError):
+        results['Speicher'].plot_charge_state(engine='matplotlib')
+
+    with pytest.raises(NotImplementedError):
+        results['Speicher'].plot_node_balance_pie(engine='matplotlib', save=save, show=show)
+
+
+def test_results_plots_plotly(simple_flow_system, save, show):
+    calculation = create_calculation_and_solve(simple_flow_system, fx.solvers.HighsSolver(0.01, 30), 'test_results_plots')
+    results = calculation.results
+
+    results['Boiler'].plot_node_balance(engine='plotly', save=save, show=show)
+
+    results.plot_heatmap('Speicher(Q_th_load)|flow_rate',
+                         heatmap_timeframes='D',
+                         heatmap_timesteps_per_frame='h',
+                         color_map='viridis',
+                         save=show,
+                         show=save,
+                         engine='matplotlib')
+
+    results['Speicher'].plot_charge_state(engine='plotly')
+
+    results['Speicher'].plot_node_balance_pie(engine='plotly', save=save, show=show)
+
+
