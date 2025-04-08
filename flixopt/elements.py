15fdd339658b9cf4e6ae673061b46b13adaf85a1
FBumann <117816358+FBumann@users.noreply.github.com>
2025-04-08 17:21:28 +0200

    Bugfix plausibility in Bus

--- flixopt/elements.py
+++ flixopt/elements.py
@@ -115,7 +115,7 @@ class Bus(Element):
         )
 
     def _plausibility_checks(self) -> None:
-        if (self.excess_penalty_per_flow_hour == 0).all():
+        if self.excess_penalty_per_flow_hour is not None and (self.excess_penalty_per_flow_hour == 0).all():
             logger.warning(f'In Bus {self.label}, the excess_penalty_per_flow_hour is 0. Use "None" or a value > 0.')
 
     @property
