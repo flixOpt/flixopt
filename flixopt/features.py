bcb9793f40c57ce9af5e8d101e974bb7ae1cb806
Felix Bumann <Felix.Bumann@sachsenenergie.de>
2025-04-22 18:55:43 +0200

    Change error to warning
    (cherry picked from commit 25dd93bb26c72eb9ccc448747badc2109f08d27e)

--- flixopt/features.py
+++ flixopt/features.py
@@ -155,9 +155,10 @@ class InvestmentModel(Model):
                 f'fix_{variable.name}',
             )
             if self._on_variable is not None:
-                raise ValueError(
-                    f'Flow {self.label_full} has a fixed relative flow rate and an on_variable.'
-                    f'This combination is currently not supported.'
+                logger.warning(
+                    f'Flow {self.label_full} has a fixed relative flow rate (or equal relative_minimum and '
+                    f'relative_maximum and an on_variable. This will allow the flow_rate to be 0 instead of the '
+                    f'specified fixed rate.'
                 )
             return
 
