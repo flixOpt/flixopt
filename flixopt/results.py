d7ba94ca064d1a120f3f0a0f21e21fb60b3cd797
FBumann <117816358+FBumann@users.noreply.github.com>
2025-04-25 12:31:37 +0200

    Bugfix: save flow_system data, not the flow_system

--- flixopt/results.py
+++ flixopt/results.py
@@ -528,7 +528,7 @@ class CalculationResults:
         paths = fx_io.CalculationResultsPaths(folder, name)
 
         fx_io.save_dataset_to_netcdf(self.solution, paths.solution, compression=compression)
-        fx_io.save_dataset_to_netcdf(self.flow_system, paths.flow_system, compression=compression)
+        fx_io.save_dataset_to_netcdf(self.flow_system_data, paths.flow_system, compression=compression)
 
         with open(paths.summary, 'w', encoding='utf-8') as f:
             yaml.dump(self.summary, f, allow_unicode=True, sort_keys=False, indent=4, width=1000)
