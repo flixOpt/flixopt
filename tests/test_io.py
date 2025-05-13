67ebfbbe75870a3f9226f9435c00748f30cc554d
FBumann <117816358+FBumann@users.noreply.github.com>
2025-05-13 11:55:29 +0200

    Scenarios/plot network (#262)
    * Catch bug in plot_network with 2D arrays
    
    * Add plot_network() to test_io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -28,6 +28,7 @@ def test_flow_system_file_io(flow_system, highs_solver):
     calculation_0 = fx.FullCalculation('IO', flow_system=flow_system)
     calculation_0.do_modeling()
     calculation_0.solve(highs_solver)
+    calculation_0.flow_system.plot_network()
 
     calculation_0.results.to_file()
     paths = CalculationResultsPaths(calculation_0.folder, calculation_0.name)
@@ -36,6 +37,7 @@ def test_flow_system_file_io(flow_system, highs_solver):
     calculation_1 = fx.FullCalculation('Loaded_IO', flow_system=flow_system_1)
     calculation_1.do_modeling()
     calculation_1.solve(highs_solver)
+    calculation_1.flow_system.plot_network()
 
     assert_almost_equal_numeric(
         calculation_0.results.model.objective.value,
