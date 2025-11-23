076dac8c42e7383efc0b23dbc3c102906efc18b0
FBumann <117816358+FBumann@users.noreply.github.com>
2025-11-23 13:04:48 +0100

    Revert some changes

--- flixopt/aggregation.py
+++ flixopt/aggregation.py
@@ -353,7 +353,7 @@ class AggregationModel(Submodel):
                 # Sum correction variables over all dimensions to get periodic penalty contribution
                 self._model.effects.add_share_to_effects(
                     name='Aggregation',
-                    expressions={PENALTY_EFFECT_LABEL: (variable * penalty).sum()},
+                    expressions={PENALTY_EFFECT_LABEL: (variable * penalty).sum('time')},
                     target='periodic',
                 )
 
@@ -373,10 +373,8 @@ class AggregationModel(Submodel):
             variable.name in self._model.variables.binaries
             and self.aggregation_parameters.percentage_of_period_freedom > 0
         ):
-            # Get unique time indices to avoid duplicate coordinates
-            unique_time_indices = np.unique(indices[0])
-            sel = variable.isel(time=unique_time_indices)
-            coords = sel.coords
+            sel = variable.isel(time=indices[0])
+            coords = {d: sel.indexes[d] for d in sel.dims}
             var_k1 = self.add_variables(binary=True, coords=coords, short_name=f'correction1|{variable.name}')
 
             var_k0 = self.add_variables(binary=True, coords=coords, short_name=f'correction0|{variable.name}')
@@ -387,10 +385,7 @@ class AggregationModel(Submodel):
             # --> correction On(p3) can be:
             #  On(p1,t) = 1 -> On(p3) can be 0 -> K0=1 (,K1=0)
             #  On(p1,t) = 0 -> On(p3) can be 1 -> K1=1 (,K0=1)
-            # Select the correction variables at the constraint time coordinates
-            # Get the time coordinates from the constrained variable selection
-            time_coords_at_indices = variable.isel(time=indices[0]).coords['time']
-            con.lhs += 1 * var_k1.sel(time=time_coords_at_indices) - 1 * var_k0.sel(time=time_coords_at_indices)
+            con.lhs += 1 * var_k1 - 1 * var_k0
 
             # interlock var_k1 and var_K2:
             # eq: var_k0(t)+var_k1(t) <= 1
